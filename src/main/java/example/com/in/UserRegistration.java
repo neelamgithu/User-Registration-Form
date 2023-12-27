7f7c03503f57c50dc7ae37bd1fcce994b3f7d878
Neelam <neelammujawar12@gmail.com>
2023-12-27 16:00:11 +0530

    Last Name check

--- src/main/java/example/com/in/UserRegistration.java
+++ src/main/java/example/com/in/UserRegistration.java
@@ -9,10 +9,30 @@ public class UserRegistration {
 	        Scanner scanner = new Scanner(System.in);
 	        String userInput = scanner.nextLine();
 	        System.out.println(isValidFirstName(userInput));
+	        
+			  System.out.print("Enter yourLast name: ");
+		        Scanner sc = new Scanner(System.in);
+		        String LastName = scanner.nextLine();
+		        System.out.println(isValidLastName(LastName));
 	}
     
 
 	
+	public static boolean isValidLastName(String LastName) {
+		 if( LastName != null && LastName.matches("[A-Z][a-zA-Z]{2,}"))
+		  {
+			  return true;
+		  }
+			
+		  else {
+			  	return false;
+       
+		  }
+		
+	}
+
+
+
 	public  static boolean isValidFirstName(String firstName) {
         // Check if the first name starts with a capital letter and has a minimum length of 3 characters
       
--- src/test/java/example/com/in/UserRegistrationTest.java
+++ src/test/java/example/com/in/UserRegistrationTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
 	@Test
-	public void should_return_true_ater_giving_first_letter_capital_()
+	public void should_return_true_after_giving_first_letter_capital_()
 	{
 		UserRegistration user =new UserRegistration();
         boolean k = user.isValidFirstName("Neelam");
         Assert.assertEquals(true,k);
-	
-		
-	
-
-}
+	}
+	@Test
+	public void should_return_true_after_giving_Last_letter_capital_()
+	{
+		UserRegistration user =new UserRegistration();
+        boolean k = user.isValidLastName("Neelam");
+        Assert.assertEquals(true,k);
+	}
 }
